dae72562b5e7caf5a74186d5c275362deffd6a08
Ahmad-Moiz <148411334+Ahmad-Moiz@users.noreply.github.com>
2023-10-21 09:05:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,30 @@
 import streamlit as st
-from transformers import pipeline, BartTokenizer, BartForConditionalGeneration
-import sumy
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lex_rank import LexRankSummarizer
-import requests
-import re
+from transformers import BartForConditionalGeneration, BartTokenizer
 
-# Load the BART model and tokenizer
+# Load the BART pre-trained model and tokenizer
 model_name = "facebook/bart-large-cnn"
 tokenizer = BartTokenizer.from_pretrained(model_name)
 model = BartForConditionalGeneration.from_pretrained(model_name)
 
-# Create a function to summarize text using the BART model
-def bart_summarize(text, max_length=150):
-    inputs = tokenizer(text, max_length=max_length, return_tensors="pt", truncation=True)
-    summary_ids = model.generate(inputs.input_ids, max_length=50, min_length=10, length_penalty=2.0, num_beams=4, early_stopping=True)
+# Define the summarization function
+def summarize_article(article):
+    # Tokenize and encode the article
+    inputs = tokenizer.encode("summarize: " + article, return_tensors="pt", max_length=1024, truncation=True)
+
+    # Generate the summary
+    summary_ids = model.generate(inputs, max_length=150, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)
+
+    # Decode and return the summary
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
-
-# Create a function to extract sentences from an article using Sumy
-def extract_sentences(text, num_sentences=3):
-    parser = PlaintextParser.from_string(text, Tokenizer("english"))
-    summarizer = LexRankSummarizer()
-    sentences = summarizer(parser.document, num_sentences)
-    return [str(sentence) for sentence in sentences]
 
 # Streamlit UI
 st.title("Article Summarizer")
 
-# Input for article URL
-article_url = st.text_input("Enter the URL of the article you want to summarize:")
+# Input box for the article
+article = st.text_area("Enter the article")
 
 if st.button("Summarize"):
-    if not article_url:
-        st.warning("Please enter a valid article URL.")
-    else:
-        try:
-            # Download the article content from the URL
-            response = requests.get(article_url)
-            article_text = response.text
-
-            # Extract sentences using Sumy
-            extracted_sentences = extract_sentences(article_text)
-
-            # Summarize the extracted sentences using BART
-            summary = bart_summarize(" ".join(extracted_sentences))
-
-            # Display the summary
-            st.subheader("Summary:")
-            st.write(summary)
-
-        except Exception as e:
-            st.error("An error occurred. Please check the URL or try again later.")
-
+    summary = summarize_article(article)
+    st.subheader("Summary:")
+    st.write(summary)
