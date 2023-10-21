c39dd38030bfa9ad8e99ead1f59d9429bc39ca32
Ahmad-Moiz <148411334+Ahmad-Moiz@users.noreply.github.com>
2023-10-21 09:32:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from transformers import pipeline
-from flask import Flask, request, jsonify
+import spacy
+import streamlit as st
 
-summarizer = pipeline("summarization")
+def article_summarizer(article_text, num_sentences=3):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(article_text)
+    sentence_importance = {}
+    for sentence in doc.sents:
+        sentence_tokens = [token for token in sentence if not token.is_stop]
+        sentence_rank = sum(token.rank for token in sentence_tokens)
+        sentence_importance[sentence] = sentence_rank
+    sorted_sentences = sorted(sentence_importance, key=lambda x: sentence_importance[x], reverse=True)
+    summary = " ".join(str(sentence) for sentence in sorted_sentences[:num_sentences])
+    return summary
 
-app = Flask(__name__)
+st.title("Article Summarizer")
 
-@app.route('/summarize', methods=['POST'])
-def summarize():
-    data = request.get_json()
-    article = data['article']
-    summary = summarizer(article, max_length=150, min_length=50, do_sample=False)
-    return jsonify({"summary": summary[0]['summary_text']})
+article = st.text_area("Enter your article here:")
+num_sentences = st.slider("Select the number of sentences for the summary:", 1, 10, 3)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if st.button("Summarize"):
+    if article:
+        summary = article_summarizer(article, num_sentences)
+        st.subheader("Summary:")
+        st.write(summary)
+    else:
+        st.warning("Please enter an article to summarize.")
