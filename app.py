74a43e7ecc05ab05baf5a85bfdd164b63c9e5616
Ahmad-Moiz <148411334+Ahmad-Moiz@users.noreply.github.com>
2023-10-21 07:30:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
 import streamlit as st
 from transformers import pipeline
 
-# Title and description
-st.title("Article Summarizer")
-st.write("Enter an article and get a summary.")
+# Create a function to summarize text using the transformers library
+def summarize_text(text):
+    summarizer = pipeline("summarization")
+    summary = summarizer(text, max_length=150, min_length=30, do_sample=False)
+    return summary[0]["summary"]
 
-# Text input
-article = st.text_area("Enter the article here:")
-
-# Check if article is provided
-if not article:
-    st.warning("Please enter an article to summarize.")
-else:
-    # Create a modal for the summary
-    with st.spinner("Summarizing..."):
-        summarizer = pipeline("summarization")
-        summary = summarizer(article, max_length=150, min_length=30, do_sample=False)
-
-    # Display the summary
-    st.subheader("Summary:")
-    st.write(summary[0]["summary"])
-
-# Optional: Add a "Learn more" link
-st.write("Learn more at [Hugging Face Transformers](https://huggingface.co/models)")
-
-# (Optional) Footer text
-st.markdown(
-    """
-    ---
-
-    Made with ❤️ by Your Name
-
-    [GitHub Repository](https://github.com/yourusername/your-repo)
-
-    """
+# Streamlit app with enhanced styling
+st.set_page_config(
+    page_title="Article Summarizer",
+    page_icon="📄",
+    layout="centered"
 )
+
+st.title("📖 Article Summarizer")
+
+# Create a button to open the summarization modal
+if st.button("Summarize Article"):
+    # Create a text area for user to input the article
+    with st.form("article_form"):
+        st.write("Paste the article you want to summarize below:")
+        article_text = st.text_area("")
+
+        # Create a button to submit the form
+        submit_button = st.form_submit_button("Summarize")
+
+    # Check if the form was submitted
+    if submit_button:
+        if article_text:
+            # Call the summarization function
+            summary = summarize_text(article_text)
+            st.subheader("Summary:")
+            st.write(summary)
+        else:
+            st.warning("Please enter an article to summarize.")
