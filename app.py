a61d5de8ba29a88115302ff1f4fa1acbfbbc2d8e
Ahmad-Moiz <148411334+Ahmad-Moiz@users.noreply.github.com>
2023-10-21 09:14:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
-import streamlit as st
-from transformers import BartForConditionalGeneration, BartTokenizer
+from transformers import pipeline
+from flask import Flask, request, jsonify
 
-# Load the BART pre-trained model and tokenizer
-model_name = "facebook/bart-large-cnn"
-tokenizer = BartTokenizer.from_pretrained(model_name)
-model = BartForConditionalGeneration.from_pretrained(model_name)
+summarizer = pipeline("summarization")
 
-# Define the summarization function
-def summarize_article(article):
-    # Tokenize and encode the article
-    inputs = tokenizer.encode("summarize: " + article, return_tensors="pt", max_length=1024, truncation=True)
+app = Flask(__name__)
 
-    # Generate the summary
-    summary_ids = model.generate(inputs, max_length=150, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.get_json()
+    article = data['article']
+    summary = summarizer(article, max_length=150, min_length=50, do_sample=False)
+    return jsonify({"summary": summary[0]['summary_text']})
 
-    # Decode and return the summary
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    return summary
-
-# Streamlit UI
-st.title("Article Summarizer")
-
-# Input box for the article
-article = st.text_area("Enter the article")
-
-if st.button("Summarize"):
-    summary = summarize_article(article)
-    st.subheader("Summary:")
-    st.write(summary)
+if __name__ == '__main__':
+    app.run(debug=True)
